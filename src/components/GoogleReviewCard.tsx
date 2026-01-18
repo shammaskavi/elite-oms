372faf4110c11fbd7d941ee7a9b4488d6eb2b972
shammaskavi <shammaskavi@gmail.com>
2026-01-18 21:17:26 +0530

    review card modified

--- src/components/GoogleReviewCard.tsx
+++ src/components/GoogleReviewCard.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Star } from "lucide-react";
 
 const GOOGLE_REVIEW_URL =
-    "https://www.google.com/search?client=safari&hs=DcBp&sca_esv=a64ff662bc0fc51f&rls=en&sxsrf=ANbL-n5Xovsbkn9OOr-0G3r2s381gaNt6w:1768728075451&si=AL3DRZFIhG6pAqfNLal55wUTwygCG0fClF3UxiOmgw9Hq7nbWYWEJrWfuSNvotMGOLkTGO1I14HlDyyHnpGNI8_Lbsqd8psnqRRxAjJFCUSXI5ta0RfBltfhvDJ6PerNvAdVRBgDUG8p7BOGAAvZqkyLTiowpVvVqg%3D%3D&q=Saree+Palace+Elite+Reviews&sa=X&ved=2ahUKEwi7z5DF4ZSSAxVwR2wGHVlDAmEQ0bkNegQINBAH&biw=1104&bih=630&dpr=2&aic=0&zx=1768728651764&no_sw_cr=1#lrd=0x395e4e995aaf186f:0xe6a14b7a6a4eee62,3,,,,";
+    "https://search.google.com/local/writereview?placeid=ChIJbxivWplOXjkRYu5OanpLoeY";
 
 export function GoogleReviewCard() {
     return (
